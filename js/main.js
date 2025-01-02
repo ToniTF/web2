b990bbaaec036717e5bb367a49a762f0313e32a6
Tonitf <subcero00@hotmail.com>
2025-01-02 19:24:22 +0100

    Boton Burguer

--- js/main.js
+++ js/main.js
@@ -7,3 +7,11 @@ document.querySelector('.imgUser').addEventListener('click', () => {
     //user.onclick=function(){} es exactamente lo mismo que addEventListener
     //Se puede crear primero la funcion con su nombre y luego llamarla en el addEventListener
     //Cuando se pone == significa comparar y === significa comparar y que sea del mismo tipo
+    var menuMovil = document.getElementById('burguer');
+    menuMovil.onclick = function(){
+        if(asideMovil.style.display == 'none'){
+            asideMovil.style.display = 'block';
+        }else{
+            asideMovil.style.display = 'none';
+        }
+    }
