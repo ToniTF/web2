148c9fffa3817e54954959fc1d604b2049b926c0
Tonitf <subcero00@hotmail.com>
2025-01-02 17:04:50 +0100

    Estilos Main

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.querySelector('.imgUser').addEventListener('click', () => { 
+    const userMenu = document.getElementById('userMenu'); 
+    userMenu.style.display = userMenu.style.display === 'none' ? 'block' : 'none'; });
+
+    //En clase se le ha puesto un id a imgUser llamado igual que la clase y se ha relizado de la siguiente manera
+    //var user=document.getElementById('imgUser');
+    //user.onclick=function(){} es exactamente lo mismo que addEventListener
+    //Se puede crear primero la funcion con su nombre y luego llamarla en el addEventListener
+    //Cuando se pone == significa comparar y === significa comparar y que sea del mismo tipo
